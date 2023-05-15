a04972ef86b281c0ba4131f3aa9d25c0ccf3b29d
最小栗子 <927552837@qq.com>
2023-05-15 15:05:09 +0800

    first commit

---
+++ bin/ssh-service.js
@@ -0,0 +1,76 @@
+const yaml = require('yaml')
+const fs = require('fs')
+const path = require('path');
+const sshListFilePath = path.join(__dirname, 'ssh-list.yml');
+
+// 如果配置文件不存在则创建
+if (!fs.existsSync(sshListFilePath)){
+    fs.writeFileSync(sshListFilePath, '', { encoding: 'utf-8' }, error => {
+        if (error) throw error;
+        console.log('ssh配置记录文件创建成功！');
+    })
+}
+
+// ssh业务对象
+
+module.exports = {
+    get,
+    all,
+    add,
+    remove
+}
+
+/**
+ * 通过别名获取ssh信息对象
+ * @param alias 别名
+ * @returns ssh信息对象
+ */
+function get(alias) {
+    return all()[alias]
+}
+
+/**
+ * 获取所有的ssh信息对象
+ * @returns 所有的ssh信息对象 可能为空
+ */
+function all() {
+    let file = fs.readFileSync(sshListFilePath, { encoding: 'utf-8' })
+    return yaml.parse(file) | {}
+}
+
+/**
+ * 添加ssh信息对象
+ * @param alias 别名
+ * @param obj ssh信息对象
+ */
+function add(alias, obj){
+    // 信息校验
+    if (!alias){
+        throw new Error('别名 -a, --alias <alias> 不能为空')
+    }
+    let allObj = all()
+    if (allObj[alias]){
+        throw new Error(`添加失败:${alias} 已经存在`)
+    }
+    allObj[alias] = obj
+    fs.writeFile(sshListFilePath, yaml.stringify(allObj), { encoding: 'utf-8' }, error => {
+        if (error) throw error;
+        console.log(`添加成功:${alias}`);
+    })
+}
+
+/**
+ * 删除ssh信息对象
+ * @param alias 别名
+ */
+function remove(alias){
+    let allObj = all()
+    if (!allObj[alias]){
+        throw new Error(`删除失败:${alias} 不存在`)
+    }
+    allObj[alias] = undefined
+    fs.writeFile(sshListFilePath, yaml.stringify(allObj), { encoding: 'utf-8' }, error => {
+        if (error) throw error;
+        console.log(`删除成功:${alias}`);
+    })
+}
---
+++ index.js
@@ -0,0 +1,86 @@
+#!/usr/bin/env node
+const program = require('commander')
+const ssh = require('ssh2-client')
+const sshService = require('./bin/ssh-service')
+
+program.version('1.0.0')
+program.command('add')
+    .option('-a, --alias <alias>', '别名')
+    .option('-h, --host <host>', '服务器地址')
+    .option('-p, --port <port>', '端口')
+    .option('-u, --username <username>', '用户名')
+    .option('-s, --password <password>', '密码')
+    .description('添加ssh记录')
+    .action(opt => {
+        try{
+            let alias = opt.alias
+            let sshObj = {
+                port: opt['port'],
+                host: opt['host'],
+                username: opt['username'],
+                password: opt['password']
+            }
+            sshService.add(alias, sshObj)
+        }catch (e){
+            console.error(e.message)
+        }
+    })
+program.command('rm <alias>')
+    .description('删除ssh记录')
+    .action(alias => {
+        try{
+            sshService.remove(alias)
+        }catch (e){
+            console.error(e.message)
+        }
+    })
+program.command('list')
+    .alias('ls')
+    .description('ssh列表')
+    .action(() => {
+        let sshObjList = sshService.all()
+        for (let alias in sshObjList){
+            console.log(alias + '\t' + sshObjList[alias].username + '@' + sshObjList[alias].host)
+        }
+    })
+program.argument('<alias>')
+    .description('通过别名访问服务器')
+    .action(alias => {
+        if (!alias){
+            program.showHelp()
+        }
+        // 第一步，通过别名找到对应的服务器信息
+        //     1.读取服务器列表配置文件
+        let sshObj = sshService.get(alias)
+        if (!sshObj){
+            console.error(`服务器${alias}未找到!`)
+            return;
+        }
+        // 第二步，如果信息存在则进行连接，否则输出信息未找到异常
+        console.log(`connect: ${alias} ...`)
+        let opts = {
+            encoding : 'utf8',
+            stdio: 'inherit'
+        }
+        ssh
+            .shell(sshObj.host, {
+                username: sshObj.username,
+                password: sshObj.password,
+                promptPassword : true
+            })
+            .catch(function onError(err) {
+                if (err.message) {
+                    process.stderr.write(err.message);
+                    process.stderr.write(err.stack);
+                } else {
+                    process.stderr.write(err);
+                }
+            })
+    })
+
+// 如果没有传递任何参数，显示帮助信息
+if (!process.argv.slice(2).length) {
+    program.help()
+}else {
+    program.parse(process.argv)
+}
